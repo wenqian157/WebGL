f414af28c924921e7876678206a43870a9018d90
wenqian157 <44199787+wenqian157@users.noreply.github.com>
2023-11-07 00:08:36 +0100

    update

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-20231106_Innovedum-0.1";
+const contentToCache = [
+    "Build/20231106_WebGLBuild.loader.js",
+    "Build/20231106_WebGLBuild.framework.js",
+    "Build/20231106_WebGLBuild.data",
+    "Build/20231106_WebGLBuild.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
